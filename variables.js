edb0978ef31d3c4ddab66abe272e753f81d09cae
Leidy Milena <milenamdiaz98@gmail.com>
2025-03-26 19:20:58 -0500

    Comit Modulo 2, video 3 variables

--- variables.js
+++ variables.js
@@ -1,1 +1,18 @@
 console.log("hello world");
+//Variables : Representación simbolica de un dato
+//Var --> Es una variable global a nivel de funcion
+var nombre ="Leidy";
+console.log(nombre);
+// let --> Es una variable local
+let apellido ="Molano";
+console.log(apellido);
+//const: variables constantes de un ambito loca
+const PI= 3.14167;
+console.log(PI);
+//Reglas Letras de la (A-Z; a-z),Numeros(0,9), Gion bajo y signo pesos
+let _nombre ="Milena";
+let $nombre = "Leidy";
+console.log(_nombre);
+console.log($nombre);
+/* No se recomienda iniciar con un numero la definicion de variables
+*/ 
