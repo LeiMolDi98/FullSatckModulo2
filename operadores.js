10f198492325fd60ecb4e82bd684a8e17c009f37
Leidy Milena <milenamdiaz98@gmail.com>
2025-03-28 18:25:10 -0500

    Modulo2. Operadores

--- operadores.js
+++ operadores.js
@@ -12,4 +12,22 @@ console.log(2 < 2);
 console.log(2 == 2);
 console.log(true);
 console.log(false);
+console.log(!false);
+
 //Ope de relación
+console.log(2 >=2);
+console.log(2 <= 2);
+console.log(20 !=2);
+
+//Ope de signación
+var numero1 = 2;
+var numero2 = 10;
+console.log(numero1 > numero2);
+console.log(numero1 < numero2);
+console.log(numero1 == numero2);
+console.log(numero1 != numero2);
+
+let numero3 = 10;
+let numero4 ="10";
+
+console.log(numero3 == numero4);
