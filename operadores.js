10edff2559328d10d73ec55c754bd745bc36dff6
Leidy Milena <milenamdiaz98@gmail.com>
2025-03-28 18:07:19 -0500

    operaciones

---
+++ operadores.js
@@ -0,0 +1,15 @@
+//Ope aritmeticos
+console.log(2 + 1);
+console.log(2 - 1);
+console.log(2 * 1);
+console.log(2 / 1);
+console.log(2 % 1);
+console.log(2 ** 2);
+
+//Ope booleanos - elementos logicos
+console.log(2 > 2);
+console.log(2 < 2);
+console.log(2 == 2);
+console.log(true);
+console.log(false);
+//Ope de relación
